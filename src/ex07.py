88b257e33d7cebd5b853194ee7d641575f8525e3
sutthipong_sangram <66056084@kmitl.ac.th>
2023-10-08 15:06:26 +0700

    Solve test_ex07

--- src/ex07.py
+++ src/ex07.py
@@ -3,7 +3,7 @@ Execise 7
 """
 
 
-def print_ASCII_table():
+def print_ASCII_table(start_char,end_char):
     """
     Generate and print the ASCII table for a range of characters.
 
@@ -16,4 +16,7 @@ def print_ASCII_table():
 
     """
     # TODO : complete this
-    pass
+    while start_char <= end_char:
+        x = chr(start_char)
+        print(x)
+        start_char += 1
