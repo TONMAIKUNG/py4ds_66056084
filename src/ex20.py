5adbadc5e34f7483a2338906e55542afe2378d63
sutthipong_sangram <66056084@kmitl.ac.th>
2023-10-13 21:21:54 +0700

    Solve test_ex20

--- src/ex20.py
+++ src/ex20.py
@@ -16,5 +16,5 @@ def is_leap_year(year):
     # TODO : complete this
     if (year % 400 == 0 ) or (year % 4 == 0) and (year % 100 != 0):
         return True
-    else :
+    else:
         return False
