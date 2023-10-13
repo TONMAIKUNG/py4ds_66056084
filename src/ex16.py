630f6711d943bb18629330aa161db1c0349caea8
sutthipong_sangram <66056084@kmitl.ac.th>
2023-10-13 18:52:32 +0700

    Solve test_ex16

--- src/ex16.py
+++ src/ex16.py
@@ -3,7 +3,7 @@ Exercise 16
 """
 
 
-def mode(params):
+def mode(num_list):
     """
     Calculate the mode of a list of numbers.
 
@@ -13,4 +13,13 @@ def mode(params):
     Returns:
     - int or None: The mode of the list, or None if the list is empty.
     """
-    pass
+    count = 0
+    if num_list.__len__()==0:
+        return None
+    else:
+        for i in num_list:
+            new_count = num_list.count(i)
+            if new_count > count:
+                count = new_count
+                mode = i
+    return mode
