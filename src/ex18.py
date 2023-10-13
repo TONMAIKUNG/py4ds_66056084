37de64f7926f3c4690d6503f95465a1f1980d0cc
sutthipong_sangram <66056084@kmitl.ac.th>
2023-10-13 19:51:32 +0700

    Solve test_ex18

--- src/ex18.py
+++ src/ex18.py
@@ -3,5 +3,17 @@ Exercise 18 : Buy 8 get 1 free
 """
 
 
-def get_cost_of_coffee(param, param1):
-    pass
+def get_cost_of_coffee(cup, price):
+    promotion_pack = 8+1
+    amount_packs = cup // promotion_pack
+    amount_cup = cup % promotion_pack
+    total = (amount_packs * 8 * price) + (amount_cup * price)
+    return total
+
+
+def get_cost_of_coffee_2(cup, price):
+    promotion_pack = 8+1
+    amount_packs = cup // promotion_pack
+    amount_cup = cup % promotion_pack
+    total = (amount_packs * 8 * price) + (amount_cup * price)
+    return total
